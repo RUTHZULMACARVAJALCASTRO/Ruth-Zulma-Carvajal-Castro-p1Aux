44c28f14ff851a94caa8d351c25ef30606916a06
RUTHZULMACARVAJALCASTRO <jeonjungkooklove321@gmail.com>
2022-10-24 06:57:24 -0400

    cambios

--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
 
-// import React from "react";
-// import { ReactDOM } from "react";
-// import { App } from "module";
-// import { BrowserRouter } from "react-router-dom";
-// import {RoutesLayoutCarvajal} from "./routes/RoutesLayoutCarvajal";
-// import './App.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
